ac776dea9b06dc28eddf6b8bbaba87d21bf6404a
Olivier <noreply4github@gmail.com>
2022-10-31 19:20:36 +0100

    feat: refactor the execute_command with a yaml config file

--- flyway.py
+++ flyway.py
@@ -1,12 +1,8 @@
 from subprocess import PIPE, Popen
 from typing import Union
-from sys import exit
-import json
+import sys
+import yaml
 
-def run_command(command: list) -> str:
-    with Popen(command, stdout=PIPE, stderr=None, shell=True) as process:
-        return process.communicate()[0].decode("utf-8")
-        
 
 class Flyway:
 
@@ -16,77 +12,83 @@ class Flyway:
         self.clean_allowed = clean_allowed
         self.environ = environ
         
-        with open(f'{self.environ}.json') as f:
-            data = json.load(f)
-            self.history_table = data['history_table']
-            self.prefix = data['prefix']
-            self.installer = data['installedBy']
-            self.url = data['url']
-            
-    def _command(self, command: Union[str, None]) -> list:
-        try:
-            command_line = [self.platform, "run", "--rm", "--network=docker-services_dbnet", self.image]
-            if command:    
-                config = [
-                    f'-table={self.history_table}',
-                    f'-sqlMigrationPrefix={self.prefix}',
-                    f'-installedBy={self.installer}',
-                    f'-user=xxxx', 
-                    '-password=xxxx', 
-                    f'-url={self.url}'] 
-                command_line = command_line + config
-                command_line.append(command)
+        with open(f'{self.environ}.yml', 'r') as stream:
+            data_loaded = yaml.safe_load(stream)
+            self.version_table = data_loaded['versionTable']
+            self.version_prefix = data_loaded['versionPrefix']
+            self.installer = data_loaded['installedBy']
+            self.database_url = data_loaded['databaseURL']
+            self.schemas = data_loaded['schemas']
+
+    def _execute_command(self, command: Union[str, None], verbose: bool) -> None:
+
+        @staticmethod
+        def run_command(command: list) -> str:
+            with Popen(command, stdout=PIPE, stderr=None, shell=True) as process:
+                return process.communicate()[0].decode("utf-8")
+
+        def _command(self, command: Union[str, None], user: Union[str, None], password: Union[str, None]) -> list:
+            try:
+                command_line = [self.platform, "run", "--rm", "--network=docker-services_dbnet", self.image]         
+                if command:  
+                    config = [
+                        f'-table={self.version_table}',
+                        f'-sqlMigrationPrefix={self.version_prefix}',
+                        f'-installedBy={self.installer}',
+                        f'-user={user}', 
+                        f'-password={password}', 
+                        f'-url={self.database_url}'] 
+                    command_line = command_line + config
+                    command_line.append(command)
+
+                if verbose:
+                    print(command_line)
+
+                return command_line
                 
-            return command_line
-        except Exception as e:
-            print(f"An error occurs with {self._command.__name__} : {e}")
-            exit(1)
+            except Exception as e:
+                print(f"An error occurs with {self._command.__name__} : {e}")
+                exit(1)
+
+        if len(self.schemas) > 0:
+            for schema in self.schemas:
+                user = self.schemas[schema]['user']
+                password = self.schemas[schema]['password']
+                print(run_command(command=_command(self, command=command, user=user, password=password)))
+        else:
+            print("There is no schema defined in the YAML file")
+            sys.exit(1)
 
     def version(self, verbose: bool=False) -> None:
         """Print the Flyway version and edition"""
-        if verbose:
-            print(self._command(self.version.__name__))
-        print(run_command(self._command(self.version.__name__)))
+        print(self._execute_command(self.version.__name__, verbose=verbose))
 
-    def help(self) -> None:
-        print(run_command(self._command(None)))
+    def help(self, verbose: bool=False) -> None:
+        print(self._execute_command(command=None, verbose=verbose))
 
     def clean(self, verbose:bool=False) -> None:
         """Drops all objects in the configured schemas"""
+        command_name = self.clean.__name__
         if not self.clean_allowed:
-            print(f"The command {self.clean.__name__} has been disable")
+            print(f"The command {command_name} has been disable")
         else:
-            if verbose:
-                print(self._command(self.clean.__name__))
-            print(run_command(self._command(self.clean.__name__)))
-
+            print(self._execute_command(command=command_name, verbose=verbose))
+                
     def info(self, verbose: bool=False) -> None:
         """Prints the information about applied, current and pending migrations"""
-        command = self._command(self.info.__name__)
-        if verbose:
-            print(command)
-        print(run_command(command))
-    
+        print(self._execute_command(command=self.info.__name__, verbose=verbose))
+        
     def migrate(self, verbose: bool=False) -> None:
         """Migrates the database"""
-        if verbose:
-            print(self._command(self.migrate.__name__))
-        print(run_command(self._command(self.migrate.__name__)))
+        print(self._execute_command(command=self.migrate.__name__, verbose=verbose))
     
     def repair(self, verbose: bool=False) -> None:
         """Repairs the schema history table"""
-        if verbose:
-            print(self._command(self.repair.__name__))
-        print(run_command(self._command(self.repair.__name__)))
+        print(self._execute_command(command=self.repair.__name__, verbose=verbose))
     
     def baseline(self, verbose: bool=False) -> None:
         """Baselines an existing database at the baselineVersion"""
-        if verbose:
-            print(self._command(self.baseline.__name__))
-        run_command(self._command(self.baseline.__name__))
-        print(run_command(self._command(self.baseline.__name__)))
-
-
+        print(self._execute_command(command=self.baseline.__name__, verbose=verbose))
 
 
 flyway = Flyway(platform="docker", image="flyway/flyway", clean_allowed=False, environ="development")
